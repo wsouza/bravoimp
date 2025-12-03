240e990e7244c5ec3b63cfd0561e2f1a0bb8290c
wsouza <tecnico@sacws.com.br>
2025-12-03 19:37:28 -0300

    importacao 2

--- model/Conexao.php
+++ model/Conexao.php
@@ -6,7 +6,7 @@ abstract class Conexao {
 
     public static function getConexao() {
         if (self::$con === null) {
-            self::$con = new PDO("pgsql:host=192.168.100.200;port=5433;dbname=aimp_lagoagrande", "postgres", "221122");            self::$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$con = new PDO("pgsql:host=192.168.100.200;port=5433;dbname=aimp_lagoagrande2", "postgres", "221122");            self::$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         return self::$con;
     }
